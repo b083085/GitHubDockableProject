fed2b04b74275e5c65a7ec4c9d978d8edbaced58
b083085 <byron_aranas@hotmail.com>
2016-12-28 11:02:31 +0800

    for the win!:)

---
+++ SampleDockableProject/SimpleViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDockableProject
+{
+    public class SimpleViewModel : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+        
+        public string Name { get; set; }
+
+        public object SimpleContent { get; set; }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected == value) 
+                    return;
+
+                _isSelected = value;
+                
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
